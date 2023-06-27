4e11ad439397d286cc39116e9b111631093847b1
Sony AK <sony@sony-ak.com>
2023-06-28 03:35:07 +0700

    Minor changes

--- src/lib/js_code.ts
+++ src/lib/js_code.ts
@@ -7,7 +7,7 @@ export const code = `// Simple demo of Monaco Editor using SvelteKit
 import { onMount, createEventDispatcher } from 'svelte';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import 'monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution';
-import 'monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution';
+// import 'monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution';
 
 let editorElement;
 let editor;
@@ -18,20 +18,11 @@ onMount(() => {
     editor = monaco.editor.create(editorElement, {
         value: 'console.log("I am the Monaco Editor");',
         language: 'javascript',
-        theme: 'vs-dark'
+        theme: 'vs-dark',
+        automaticLayout: true,
     });
 
     editor.onDidChangeModelContent(() => {
         dispatch('input', editor.getValue());
     });
-
-    const handleResize = () => {
-        editor.layout();
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-        window.removeEventListener('resize', handleResize);
-    };
 });`;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 	import 'monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution';
 	// import 'monaco-editor/esm/vs/basic-languages/typescript/typescript.contribution'; // uncomment this line to enable typescript
+	// import 'monaco-editor/esm/vs/basic-languages/html/html.contribution'; // uncomment this line to enable html
 	import { code } from '$lib/js_code';
 
 	let editorElement: HTMLElement;
@@ -14,22 +15,13 @@
 		editor = monaco.editor.create(editorElement, {
 			value: code,
 			language: 'javascript',
-			theme: 'vs-dark'
+			theme: 'vs-dark',
+			automaticLayout: true
 		});
 
 		editor.onDidChangeModelContent(() => {
 			dispatch('input', editor.getValue());
 		});
-
-		const handleResize = () => {
-			editor.layout();
-		};
-
-		window.addEventListener('resize', handleResize);
-
-		return () => {
-			window.removeEventListener('resize', handleResize);
-		};
 	});
 </script>
 
